2ed8c6c8e5c0701ff229cf3bc562355b7e7f7ae2
boliwar <boliwar@gmail.com>
2022-11-12 21:08:20 +0200

    На сайте будет правильный возраст винодельни

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+
+
+birth_year = 1920
+now_year = int(datetime.datetime.now().strftime('%Y'))
+together_years = now_year - birth_year
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+
+
+rendered_page = template.render(
+    together_years=together_years,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
